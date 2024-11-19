ab5e93b0e20a72f6f58f0eff47794c2a60ce2837
DanIzunna <105650690+DanIzunna@users.noreply.github.com>
2024-11-19 10:04:34 +0000

    Update SaltSense.py added charting

--- SaltSense.py
+++ SaltSense.py
@@ -1,27 +1,39 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, jsonify, redirect
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", name="index", title="Home")
-
-@app.route("/desalinization")
-def desalinization():
-    return render_template("Desalinization.html", name="Desalinization", title="Desalinization")
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template("About.html", name="About", title="About")
+    return render_template("About.html")
 
-@app.route("/data-and-statistics")
-def data_and_statistics():
-    return render_template("Data-and-Statistics.html", name="Data-and-Statistics", title="Data And Statistics")
+@app.route("/faq")
+def faq():
+    return render_template("FAQ.html", name="FAQ", title="FAQ")
 
-# TODO: Ask how to do this.
-# @app.route("/faq")
-# def faq():
-#     return render_template("FAQ.html", name="FAQ", title="FAQ")
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+
+@app.route('/data')
+def get_data():
+    file_path = os.path.join(app.root_path, 'static', 'desalination_data.csv')
+    df = pd.read_csv(file_path)
+    data = df.to_dict(orient='records')
+    return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
