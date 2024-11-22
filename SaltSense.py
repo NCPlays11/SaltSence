b7cfe212b6355fb3672ab2cb3bbe33a8411122b4
DanIzunna <105650690+DanIzunna@users.noreply.github.com>
2024-11-22 12:51:40 +0000

    Update SaltSense.py

--- SaltSense.py
+++ SaltSense.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, jsonify, redirect
+from flask import Flask, render_template, jsonify, redirect, flash, request, url_for
 import pandas as pd
 import os
+import csv
 
 app = Flask(__name__)
+
+# app.secret_key = os.urandom(64)
+
+app.secret_key = 'FTYUJLGTCKJHBVijufewnjv832ur5456e4w58hvb4o5'
 
 @app.route("/")
 def home():
@@ -34,6 +39,26 @@ def get_data():
     df = pd.read_csv(file_path)
     data = df.to_dict(orient="records")
     return jsonify(data)
+
+# Subscribe
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    email = request.form.get('email')  
+    if email:
+        # Define the file path within the 'static' directory
+        file_path = os.path.join(app.root_path, "static", "subscribers.csv")
+        # Save the email to the CSV file
+        with open(file_path, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([email])
+        
+        # Flash a success message
+        flash('You have successfully subscribed!', 'success')
+    else:
+        flash('Please provide a valid email address.', 'danger')
+    
+    return redirect(url_for('home'))
+
 
 @app.errorhandler(404)
 def page_not_found(error):
